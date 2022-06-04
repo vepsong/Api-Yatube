b55865880ad5911ac1a2df708f9bd2af47681cc9
Dmitry Kirsanov <dmitkirsanov@yandex.ru>
2022-06-04 23:21:47 +0300

    08

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -18,23 +18,6 @@ class AuthorOrReadOnly(permissions.BasePermission):
             return False
 
 
-class AdminOrReadOnly(permissions.BasePermission):
-
-    def has_permission(self, request, view):
-        return (
-            request.method in permissions.SAFE_METHODS
-            or request.user.is_superuser
-        )
-
-    def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS:
-            return True
-        elif request.user.is_superuser:
-            return True
-        else:
-            return False
-
-
 class UserOrIsauthenticated(permissions.BasePermission):
 
     def has_permission(self, request, view):
